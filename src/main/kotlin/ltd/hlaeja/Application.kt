8a0aff4729ca4257c561859289e95bed70029725
Swordsteel <swordsteel@gmail.com>
2024-11-22 12:36:50 +0100

    add JwtProperty

--- src/main/kotlin/ltd/hlaeja/Application.kt
+++ src/main/kotlin/ltd/hlaeja/Application.kt
@@ -1,8 +1,11 @@
 package ltd.hlaeja
 
+import ltd.hlaeja.property.JwtProperty
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(JwtProperty::class)
 @SpringBootApplication
 class Application
 
