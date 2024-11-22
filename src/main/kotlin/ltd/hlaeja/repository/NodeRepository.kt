1dfadca2be2ae387e47345337a2f4c9087af870c
Swordsteel <swordsteel@gmail.com>
2024-11-22 23:30:14 +0100

    add findByDevice to NodeRepository

--- src/main/kotlin/ltd/hlaeja/repository/NodeRepository.kt
+++ src/main/kotlin/ltd/hlaeja/repository/NodeRepository.kt
@@ -2,8 +2,13 @@ package ltd.hlaeja.repository
 
 import java.util.UUID
 import ltd.hlaeja.entity.NodeEntity
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface NodeRepository : CoroutineCrudRepository<NodeEntity, UUID>
+interface NodeRepository : CoroutineCrudRepository<NodeEntity, UUID> {
+
+    @Query("SELECT * FROM nodes WHERE device = :device")
+    suspend fun findByDevice(device: UUID): NodeEntity?
+}
