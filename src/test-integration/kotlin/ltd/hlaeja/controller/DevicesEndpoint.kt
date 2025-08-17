14d36f21db21acbf0ab88d4167ec4cd60dac5236
Swordsteel <swordsteel@gmail.com>
2025-08-17 19:16:26 +0200

    add devices by type endpoint
    - update DevicesController
    
      - update DevicesEndpoint
      - update DevicesControllerTest
      - add getDevicesByType to DevicesController
      - update devices.http
    
    - add getDevicesByType to DeviceService
    
    - add findAllByType to DeviceRepository

--- src/test-integration/kotlin/ltd/hlaeja/controller/DevicesEndpoint.kt
+++ src/test-integration/kotlin/ltd/hlaeja/controller/DevicesEndpoint.kt
@@ -1,9 +1,11 @@
 package ltd.hlaeja.controller
 
+import java.util.UUID
 import ltd.hlaeja.library.deviceRegistry.Devices
 import ltd.hlaeja.test.container.PostgresTestContainer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -27,79 +29,173 @@ class DevicesEndpoint {
         webClient = WebTestClient.bindToServer().baseUrl("http://localhost:$port").build()
     }
 
-    @Test
-    fun `get devices default - success`() {
-        // when
-        val result = webClient.get().uri("/devices").exchange()
+    @Nested
+    inner class GetDevices {
 
-        // then
-        result.expectStatus().isOk()
-            .expectBody<List<Devices.Response>>()
-            .consumeWith {
-                assertThat(it.responseBody?.size).isEqualTo(4)
-            }
+        @Test
+        fun `get devices default - success`() {
+            // when
+            val result = webClient.get().uri("/devices").exchange()
+
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(4)
+                }
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "1,4",
+                "2,0",
+            ],
+        )
+        fun `get devices by page - success`(page: Int, expected: Int) {
+            // when
+            val result = webClient.get().uri("/devices/page-$page").exchange()
+
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(expected)
+                }
+        }
+
+        @Test
+        fun `get devices by pages - fail`() {
+            // when
+            val result = webClient.get().uri("/devices/page-0").exchange()
+
+            // then
+            result.expectStatus().isBadRequest
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "1,2,2",
+                "2,2,2",
+                "3,2,0",
+            ],
+        )
+        fun `get devices by page and show - success`(page: Int, show: Int, expected: Int) {
+            // when
+            val result = webClient.get().uri("/devices/page-$page/show-$show").exchange()
+
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(expected)
+                }
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0,1",
+                "1,0",
+            ],
+        )
+        fun `get devices by page and show - fail`(page: Int, show: Int) {
+            // when
+            val result = webClient.get().uri("/devices/page-$page/show-$show").exchange()
+
+            // then
+            result.expectStatus().isBadRequest
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "1,4",
-            "2,0",
-        ],
-    )
-    fun `get devices by page - success`(page: Int, expected: Int) {
-        // when
-        val result = webClient.get().uri("/devices/page-$page").exchange()
+    @Nested
+    inner class GetDevicesByType {
 
-        // then
-        result.expectStatus().isOk()
-            .expectBody<List<Devices.Response>>()
-            .consumeWith {
-                assertThat(it.responseBody?.size).isEqualTo(expected)
-            }
-    }
+        @Test
+        fun `get devices for type default - success`() {
+            // when
+            val result = webClient.get()
+                .uri("/devices/type-00000000-0000-0000-0001-000000000001")
+                .exchange()
 
-    @Test
-    fun `get devices by pages - fail`() {
-        // when
-        val result = webClient.get().uri("/devices/page-0").exchange()
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(2)
+                }
+        }
 
-        // then
-        result.expectStatus().isBadRequest
-    }
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "00000000-0000-0000-0001-000000000001,1,2",
+                "00000000-0000-0000-0001-000000000001,2,0",
+            ],
+        )
+        fun `get devices for type by page - success`(type: UUID, page: Int, expected: Int) {
+            // when
+            val result = webClient.get()
+                .uri("/devices/type-$type/page-$page")
+                .exchange()
 
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "1,2,2",
-            "2,2,2",
-            "3,2,0",
-        ],
-    )
-    fun `get devices by page and show - success`(page: Int, show: Int, expected: Int) {
-        // when
-        val result = webClient.get().uri("/devices/page-$page/show-$show").exchange()
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(expected)
+                }
+        }
 
-        // then
-        result.expectStatus().isOk()
-            .expectBody<List<Devices.Response>>()
-            .consumeWith {
-                assertThat(it.responseBody?.size).isEqualTo(expected)
-            }
-    }
+        @Test
+        fun `get devices for type by pages - fail`() {
+            // when
+            val result = webClient.get()
+                .uri("/devices/type-00000000-0000-0000-0001-000000000001/page-0")
+                .exchange()
 
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "0,1",
-            "1,0",
-        ],
-    )
-    fun `get devices by page and show - fail`(page: Int, show: Int) {
-        // when
-        val result = webClient.get().uri("/devices/page-$page/show-$show").exchange()
+            // then
+            result.expectStatus().isBadRequest
+        }
 
-        // then
-        result.expectStatus().isBadRequest
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "00000000-0000-0000-0001-000000000001,1,1,1",
+                "00000000-0000-0000-0001-000000000001,2,1,1",
+                "00000000-0000-0000-0001-000000000001,3,1,0",
+            ],
+        )
+        fun `get devices for type by page and show - success`(type: UUID, page: Int, show: Int, expected: Int) {
+            // when
+            val result = webClient.get()
+                .uri("/devices/type-$type/page-$page/show-$show")
+                .exchange()
+
+            // then
+            result.expectStatus().isOk()
+                .expectBody<List<Devices.Response>>()
+                .consumeWith {
+                    assertThat(it.responseBody?.size).isEqualTo(expected)
+                }
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0,1",
+                "1,0",
+            ],
+        )
+        fun `get devices for type by page and show - fail`(page: Int, show: Int) {
+            // when
+            val result = webClient.get()
+                .uri("/devices/type-00000000-0000-0000-0001-000000000001/page-$page/show-$show")
+                .exchange()
+
+            // then
+            result.expectStatus().isBadRequest
+        }
     }
 }
